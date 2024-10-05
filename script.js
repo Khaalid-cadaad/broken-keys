02854209b573dc1115c499ad8442cf1c7a8df722
unknown <khalidabdi3101@gmail.com>
2024-10-05 05:57:27 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+function brokenKeys(correct, typed) {
+   
+    const broken = new Set();
+
+    
+    for (let i = 0; i < correct.length; i++) {
+        
+        if (correct[i] !== typed[i]) {
+           
+            broken.add(correct[i]);
+        }
+    }
+
+    
+    return Array.from(broken);
+}
+
+
+console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp")); // ➞ ["a", "t"]
+console.log(brokenKeys("good night", "gppd night")); // ➞ ["o"]
